5fec777f2e7f64fcabd291efad14a87f0ca5cb6d
Paulo <paulo_piotto@hotmail.com>
2021-11-28 21:20:11 -0300

    EsLint implementado

--- src/controllers/signIn.js
+++ src/controllers/signIn.js
@@ -1,34 +1,32 @@
-import connection from "../database/database.js";
-import bcrypt from "bcrypt";
-import { v4 as uuid } from 'uuid'
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import connection from '../database/database.js';
 
-async function logUser(req, res){
-   const {
-       email,
-       password
-   } = req.body;
+async function logUser(req, res) {
+  const {
+    email,
+    password,
+  } = req.body;
 
-   try {
-       const result = await connection.query('SELECT * FROM users WHERE email = $1', [email]);
-       const user = result.rows[0];
+  try {
+    const result = await connection.query('SELECT * FROM users WHERE email = $1', [email]);
+    const user = result.rows[0];
 
-       if(user && bcrypt.compareSync(password, user.password)){
-           const token = uuid();
-           await connection.query('INSERT INTO sessions ("userId", token) VALUES ($1, $2)', [user.id, token]);
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = uuid();
+      await connection.query('INSERT INTO sessions ("userId", token) VALUES ($1, $2)', [user.id, token]);
 
-           const rows = await connection.query('SELECT sessions.token, users.name FROM sessions JOIN users ON sessions."userId" = users.id WHERE token = $1', [token]);
-           const userData = rows.rows[0];
-           res.send(userData).status(200);
-       }else{
-           res.sendStatus(401);
-       }
-   } catch (error) {
-       res.sendStatus(500);
-   }
+      const rows = await connection.query('SELECT sessions.token, users.name FROM sessions JOIN users ON sessions."userId" = users.id WHERE token = $1', [token]);
+      const userData = rows.rows[0];
+      res.send(userData).status(200);
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (error) {
+    res.sendStatus(500);
+  }
 }
 
-export{
-    logUser,
-}
-
-//ab359f59-a4f2-444d-a079-b26935875e9d
+export {
+  logUser,
+};
--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import './setup.js';
 import app from './app.js';
 
 app.listen(4000, () => {
-  console.log("Server running on port 4000");
+  console.log('Server running on port 4000');
 });
